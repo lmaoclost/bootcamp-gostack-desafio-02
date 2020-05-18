8199f2628f893beb2fc07b1924fe71f2e6f0381e
lmaoclost <renansmoliveira@gmail.com>
2020-05-18 11:46:47 -0300

    Recipient Model, Controller, Validation and Routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
+import RecipientController from './app/controller/RecipientController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -8,7 +9,14 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+// Todas as Rotas abaixo necessitam do Token de acesso
 routes.use(authMiddleware);
+
+// Usuários
 routes.put('/users', UserController.update);
+
+// Destinatarios
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
 
 export default routes;
